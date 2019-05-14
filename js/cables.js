10a33a82d0df9cc6543ce70e37faa0c48157ec32
KIMSENG TE <kimsengtebuy@gmail.com>
2019-05-14 15:27:52 +0700

    Initial website

---
+++ js/cables.js
@@ -0,0 +1,127 @@
+function myFunction1(imgs) {
+  var expandImg = document.getElementById("expandedImg1");
+  var imgText = document.getElementById("imgtext1");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction2(imgs) {
+  var expandImg = document.getElementById("expandedImg2");
+  var imgText = document.getElementById("imgtext2");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction3(imgs) {
+  var expandImg = document.getElementById("expandedImg3");
+  var imgText = document.getElementById("imgtext3");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction4(imgs) {
+  var expandImg = document.getElementById("expandedImg4");
+  var imgText = document.getElementById("imgtext4");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction5(imgs) {
+  var expandImg = document.getElementById("expandedImg5");
+  var imgText = document.getElementById("imgtext5");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction6(imgs) {
+  var expandImg = document.getElementById("expandedImg6");
+  var imgText = document.getElementById("imgtext6");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction7(imgs) {
+  var expandImg = document.getElementById("expandedImg7");
+  var imgText = document.getElementById("imgtext7");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction8(imgs) {
+  var expandImg = document.getElementById("expandedImg8");
+  var imgText = document.getElementById("imgtext8");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction9(imgs) {
+  var expandImg = document.getElementById("expandedImg9");
+  var imgText = document.getElementById("imgtext9");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction10(imgs) {
+  var expandImg = document.getElementById("expandedImg10");
+  var imgText = document.getElementById("imgtext10");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction11(imgs) {
+  var expandImg = document.getElementById("expandedImg11");
+  var imgText = document.getElementById("imgtext11");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction12(imgs) {
+  var expandImg = document.getElementById("expandedImg12");
+  var imgText = document.getElementById("imgtext12");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction13(imgs) {
+  var expandImg = document.getElementById("expandedImg13");
+  var imgText = document.getElementById("imgtext13");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction14(imgs) {
+  var expandImg = document.getElementById("expandedImg14");
+  var imgText = document.getElementById("imgtext14");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction15(imgs) {
+  var expandImg = document.getElementById("expandedImg15");
+  var imgText = document.getElementById("imgtext15");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+function myFunction16(imgs) {
+  var expandImg = document.getElementById("expandedImg16");
+  var imgText = document.getElementById("imgtext16");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
